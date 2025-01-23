bc686237914e2997f3018187523840bb868cfae7
sjaspal01 <jaspal0113@gmail.com>
2025-01-24 04:07:49 +0530

    Edit blacklist

--- models/blacklist.model.js
+++ models/blacklist.model.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
+
+const blacklistSchema = mongoose.Schema({
+     token : String
+})
+
+
+const Blacklist = mongoose.model("blacklist",blacklistSchema)
+
+
+module.exports={
+    Blacklist
+}
 
